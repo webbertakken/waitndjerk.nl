5b10af9056f21b1c5f738d564d4632b354ea4051
Webber <webber@takken.io>
2022-12-04 00:17:29 +0100

    feat: images until 24

--- src/theme/Image/index.tsx
+++ src/theme/Image/index.tsx
@@ -4,13 +4,19 @@ import styles from './index.module.css';
 interface Props {
   src: string;
   alt: string;
+  year?: number;
 }
 
-const Image = ({ src, alt, ...props }: Props): JSX.Element => {
+const Image = ({ src, alt, year, ...props }: Props): JSX.Element => {
   return (
     <div className={styles.wrapper}>
       <img className={styles.image} {...props} src={src} alt={alt} />
-      {alt && <em className={styles.title}>{alt}.</em>}
+      {alt && (
+        <em className={styles.title}>
+          {alt}
+          {year && ` (${year})`}.
+        </em>
+      )}
     </div>
   );
 };
